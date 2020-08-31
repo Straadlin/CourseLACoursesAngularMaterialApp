11c76d738e77bdbba23830c63f192e9f20de6eaa
Alfredo Estrada <alfredo.estrada@straad.mx>
2020-08-31 00:50:59 -0500

    Forms (Checkbox)
    Based on video:
    https://ed.team/cursos/angular-material/03/05

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -18,6 +18,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [],
@@ -39,7 +40,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatInputModule,
     MatSelectModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatCheckboxModule
   ],
   exports:[
     CommonModule,
@@ -59,7 +61,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatInputModule,
     MatSelectModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatCheckboxModule
   ]
 })
 export class MaterialModule { }
