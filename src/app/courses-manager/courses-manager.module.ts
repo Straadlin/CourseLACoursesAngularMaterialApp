a14e83cd1e8bdc29f81abb988f4ea4a3f11fc65a
Alfredo Estrada <alfredo.estrada@straad.mx>
2020-09-03 18:40:33 -0500

    Layout components (Tree)
    Based on video:
    https://ed.team/cursos/angular-material/04/05
    https://ed.team/cursos/angular-material/04/06

--- src/app/courses-manager/courses-manager.module.ts
+++ src/app/courses-manager/courses-manager.module.ts
@@ -11,10 +11,11 @@ import { FormsModule } from '@angular/forms';
 import { CourseListComponent } from './components/course-list/course-list.component';
 import { CardsComponent } from './components/course-list/cards/cards.component';
 import { LisComponent } from './components/course-list/lis/lis.component';
+import { TreeComponent } from './components/course-list/tree/tree.component';
 
 
 @NgModule({
-  declarations: [CoursesManagerComponent, SidenavComponent, ToolbarComponent, CourseAddComponent, CourseListComponent, CardsComponent, LisComponent],
+  declarations: [CoursesManagerComponent, SidenavComponent, ToolbarComponent, CourseAddComponent, CourseListComponent, CardsComponent, LisComponent, TreeComponent],
   imports: [
     CommonModule,
     CoursesManagerRoutingModule,
