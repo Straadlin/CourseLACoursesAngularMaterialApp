2bff0ec147b73bbae7d3afdd9bc1df438e22252b
Alfredo Estrada <alfredo.estrada@straad.mx>
2020-09-04 00:34:20 -0500

    Filtering data
    Based on video:
    https://ed.team/cursos/angular-material/05/02

--- src/app/courses-manager/components/course-list/table/table.component.ts
+++ src/app/courses-manager/components/course-list/table/table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from 'src/app/courses-manager/services/course.service';
 import { Course } from 'src/app/courses-manager/models/course';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-table',
@@ -11,13 +12,16 @@ export class TableComponent implements OnInit {
 
   //displayedColumns: string[] = ['id', 'name', 'description', 'speciality'];
   displayedColumns: string[] = ['name', 'description', 'speciality'];
-  dataSource: Course[];
+  dataSource = new MatTableDataSource<Course>();
 
   constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
     this.courseService.getCourses()
-    .then(courses => this.dataSource = courses);
+    .then(courses => this.dataSource = new MatTableDataSource(courses));
   }
 
+  applyFilter(text: string) {
+    this.dataSource.filter = text.trim().toLocaleLowerCase();
+  }
 }
